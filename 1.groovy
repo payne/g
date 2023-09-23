e9b8ea819edcffa17f5967806ba6aacee3cbd7ed
Matt Payne <Payne@MattPayne.org>
2023-09-22 20:04:44 -0500

    bustedd

--- 1.groovy
+++ 1.groovy
@@ -1,1 +1,22 @@
+@Grab('com.xlson.groovycsv:groovycsv:1.3')
+import static com.xlson.groovycsv.CsvParser.parseCsv
+
 println("Hello, Groovy!");
+
+
+personCSV="""
+ID,Product,Location
+HA5386,50000.00,Omaha-504
+HA5387,70000.00,Omaha-504
+HA5388,80000.00,Omaha-504
+"""
+
+println(personCSV);
+
+
+def data = parseCsv(personCSV)
+for(line in data) {
+    println "ID=$line.ID, Product=$line.Product"
+}
+
+
